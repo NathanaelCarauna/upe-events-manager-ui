db8069c9de9731961581b1758c71ddf74f9fd5ab
NathanaelCarauna <Nathanael.carauna@gmail.com>
2024-06-19 21:11:42 -0300

    Front-00 - Feat: Criar componente para tratamento de excećões

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,32 +1,47 @@
-import Button from '@mui/material/Button';
-import DemoPaper from '@mui/material/Paper'
-import Event from '@mui/icons-material/Event';
-import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import Button from "@mui/material/Button";
+import DemoPaper from "@mui/material/Paper";
+import Event from "@mui/icons-material/Event";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
+import { toast } from "react-toastify";
+import ErrorService from "../services/errorService";
 
-function Dashboard(){
-    return(
-        <>
-        <a href="/eventos">
-            <Button>
-                <DemoPaper square={false}>
-                    <Event/>
-                    <p>Eventos</p>
-                </DemoPaper>
-            </Button>
-        </a>
+function Dashboard() {
+    
+  const handleSomeAction = async () => {
+    const error = {
+      response: {
+        status: 500,
+        data: {
+          message: "Erro interno do servidor",
+        },
+      },
+    };
+    ErrorService.handleError(error);
+  };
 
-        <a href="/metricas">
-            <Button>
-                <DemoPaper square={false}>
-                    <FormatListNumberedIcon/>
-                    <p>Métricas</p>
-                </DemoPaper>
-            </Button>
-        </a>
+  return (
+    <>
+      <a href="/eventos">
+        <Button>
+          <DemoPaper square={false}>
+            <Event />
+            <p>Eventos</p>
+          </DemoPaper>
+        </Button>
+      </a>
 
-        
-        </>
-    )
+      <a href="/metricas">
+        <Button>
+          <DemoPaper square={false}>
+            <FormatListNumberedIcon />
+            <p>Métricas</p>
+          </DemoPaper>
+        </Button>
+      </a>
+
+      <button onClick={handleSomeAction}>Clique para acionar erro</button>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
