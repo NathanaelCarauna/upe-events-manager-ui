43bbbb69b85a7cd8e888a49f375a15bc5474cd52
netinhofamozo <mjan@poli.br>
2024-06-24 19:20:42 -0300

    feat: filtro para papers

--- src/pages/Papers.js
+++ src/pages/Papers.js
@@ -1,32 +1,148 @@
 import React, { useEffect, useState } from 'react';
 import api from '../services/api';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Button, Dialog, DialogActions,
-  DialogContent, DialogContentText, DialogTitle, TextField, Alert, CircularProgress, Box } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Select, Button, TextField,
+   CircularProgress, TablePagination,MenuItem} from '@mui/material';
 
 function Papers () {
   const [papers, setPapers] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [areas, setAreas] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [showPapers, setShowPapers] = useState(false);
+  const [search,setSearch] = useState(null);
+  const [area,setArea] = useState(null);
+  const [eventId,setEventId] = useState(null);
+  const [page,setPage] = useState(0);
+  const [rowsPerPage,setRowsPerPage] = useState(10);
+  const [papersCount,setPapersCount] = useState(0);
 
-  const fetchPapers = async () => {
+  const handleSelectEventChange = (event) => {
+    setEventId(event.target.value);
+  };
+
+  const handleSelectAreaChange = (event) => {
+    setArea(event.target.value);
+  };
+
+  const handleRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  }
+
+  const handleChangePage = (event,newPage) => {
+    setPage(newPage);
+  }
+
+  const handleSearch = () => {
+    const filters = {};
+
+    filters.page = page + 1;
+    filters.page_size = rowsPerPage;
+
+    if (eventId) filters.event_id = eventId;
+    if (area) filters.area = area;
+    if (search) filters.search = search;
+
+    fetchPapers(filters);
+};
+
+const clearParams = () => {
+  setSearch(null);
+  setArea(null);
+  setEventId(null);
+
+  const filters = {};
+  filters.page = 1;
+  filters.page_size = rowsPerPage;
+
+  fetchPapers(filters);
+};
+
+
+  const fetchPapers = async (filters = {}) => {
     setLoading(true);
-    try {
-      const response = await api.get('/papers');
+    setLoading(true);
+    api.get('/papers',{ params: filters}).then((response) => {
       setPapers(response.data.papers);
       setLoading(false);
       setShowPapers(true);
-    } catch (err) {
-      setError(err);
+    }).catch((error) => {
+      setError(error);
       setLoading(false);
-    }
-  };
+    })
+};
+
+const fetchEvents = async () => {
+  try {
+      const response = await api.get('/events');
+      setEvents(response.data.events);
+  } catch (err) {
+      setError(err);
+  }
+};
+
+const fetchAreas = async () => {
+  try {
+      const response = await api.get('/papers/areas');
+      setAreas(response.data.areas);
+  } catch (err) {
+      setError(err);
+  }
+};
+
+  useEffect (() => {  
+    const filters = {};
+    filters.page = 1;
+    filters.page_size = rowsPerPage;
+    fetchPapers(filters);
+    setPapersCount(papers.length || 0);
+  },[,rowsPerPage,page]);
+
 
   return (
-    <Box>
+    <Paper sx={{width: '90%', marginLeft:'5%', marginTop:'5%'}}>
       <Typography variant="h6" gutterBottom component="div" align="center" fontSize={'40px'} style={{ padding: '20px' }}>
         Listagem de Artigos
       </Typography>
+      <Select
+          defaultValue=""
+          value={eventId}
+          label="Evento"
+          onChange={handleSelectEventChange}
+          onOpen={fetchEvents}
+        >
+          {events.map((event) => (
+            <MenuItem value={event.id} key={event.id}>
+              {event.name}
+            </MenuItem>
+          ))}
+        </Select>
+        <Select
+          defaultValue=""
+          value={area}
+          label="Area"
+          onChange={handleSelectAreaChange}
+          onOpen={fetchAreas}
+        >
+          {areas.map((area) => (
+            <MenuItem value={area} key={area}>
+              {area}
+            </MenuItem>
+          ))}
+        </Select>
+        <TextField
+            label="Busca"
+            defaultValue=""
+            variant="outlined"
+            InputLabelProps={{ 
+              shrink: true
+            }}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+        />
+        <Button onClick={handleSearch}>Filtrar</Button>
+        <Button onClick={clearParams}>Limpar filtros</Button>
       <TableContainer component={Paper}>
         {loading && <CircularProgress />}
         {error && <Typography color="error" align="center">Erro ao carregar dados: {error.message}</Typography>}
@@ -39,7 +155,6 @@ function Papers () {
                   <TableCell>Título</TableCell>
                   <TableCell>Autores</TableCell>
                   <TableCell>Área</TableCell>
-                  <TableCell>Ignorar</TableCell>
                   <TableCell>Total de Páginas</TableCell>
                 </TableRow>
               </TableHead>
@@ -50,35 +165,26 @@ function Papers () {
                     <TableCell>{paper.title}</TableCell>
                     <TableCell>{paper.authors}</TableCell>
                     <TableCell>{paper.area}</TableCell>
-                    <TableCell>{paper.is_ignored ? "Sim" : "Não"}</TableCell>
                     <TableCell>{paper.total_pages}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
-            {papers.length > 0 && papers[0].event && (
-              <Box display="flex" justifyContent="center" style={{ margin: '16px' }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  href={`https://s3.amazonaws.com/${papers[0].event.s3_folder_name}/${papers[0].event.anal_filename}`}
-                  target="_blank"
-                >
-                  Gerar Anais
-                </Button>
-              </Box>
-            )}
           </>
         )}
       </TableContainer>
-      <Button
-                variant="contained"
-                color="primary"
-                onClick={fetchPapers}
-            >
-            Carregar Anais
-            </Button>
-    </Box>
+      <TablePagination
+        page={page}
+        rowsPerPage={rowsPerPage}
+        component="div"
+        onPageChange={handleChangePage}
+        count={papersCount}
+        labelRowsPerPage="Colunas por página:"
+        rowsPerPageOptions={[5,10,20]}
+        onRowsPerPageChange={handleRowsPerPage}
+        >
+      </TablePagination>
+    </Paper>
   );
 };
 
