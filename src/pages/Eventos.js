4b92ff462166d569a525a998ad886b48cf899101
lgr4 <lgr1@poli.br>
2024-06-24 15:32:54 -0300

    [Front-03] - Ajustes na filtragem de eventos

--- src/pages/Eventos.js
+++ src/pages/Eventos.js
@@ -5,120 +5,105 @@ import ModalPapers from '../components/ModalPapers';
 
 function Eventos() {
     const [events, setEvents] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [filters, setFilters] = useState({
-        tempName: '',
-        tempPromotedBy: '',
-        tempInitialDate: '',
-        tempFinalDate: '',
-        name: '',
-        promotedBy: '',
-        initialDate: '',
-        finalDate: ''
-    });
     const [showTable, setShowTable] = useState(false);
 
-    const fetchEvents = async () => {
+    const [nome, setNome] = useState('');
+    const [dataInicio, setDataInicio] = useState('');
+    const [dataFim, setDataFim] = useState('');
+    const [promovidoPor, setPromovidoPor] = useState('');
+
+    const fetchEvents = async (filters = {}) => {
         setLoading(true);
         try {
-            const response = await api.get('/events');
+            const response = await api.get('/events', { params: filters });
             setEvents(response.data.events);
+            setLoading(false);
         } catch (err) {
             setError(err);
-        } finally {
             setLoading(false);
         }
+    };
+
+    const handleSearch = () => {
+        const filters = {};
+
+        if (nome) filters.name = nome;
+        if (promovidoPor) filters.promoted_by = promovidoPor;
+        if (dataInicio) filters.initial_date = formatar_backend(dataInicio);
+        if (dataFim) filters.final_date = formatar_backend(dataFim);
+
+        fetchEvents(filters);
+        setShowTable(true);
+    };
+
+    const formatar_backend = (date) => {
+        const [year, month, day] = date.split('-');
+        return `${day}-${month}-${year}`;
     };
 
     useEffect(() => {
         fetchEvents();
     }, []);
 
-    const handleFilter = () => {
-        setFilters({
-            ...filters,
-            name: filters.tempName,
-            promotedBy: filters.tempPromotedBy,
-            initialDate: filters.tempInitialDate,
-            finalDate: filters.tempFinalDate
-        });
-        setShowTable(true);
-    };
-
-    const format_date = (dateStr) => {
-        const [day, month, year] = dateStr.split('-');
-        return `${year}-${month}-${day}`;
-    };
-
-    const filteredEvents = events.filter(event => {
-        const initialDate = event.initial_date ? format_date(event.initial_date) : '';
-        const finalDate = event.final_date ? format_date(event.final_date) : '';
-        return (
-            event.name.toLowerCase().includes(filters.name.toLowerCase()) &&
-            event.promoted_by.toLowerCase().includes(filters.promotedBy.toLowerCase()) &&
-            (filters.initialDate === '' || initialDate === filters.initialDate) &&
-            (filters.finalDate === '' || finalDate === filters.finalDate)
-        );
-    });
-
     return (
-        <Box sx={{ pt: 5, pb: 4, pl: 10, pr: 10 }}>
+        <Box sx={{ pt: 5, pb: 2, pl: 10, pr: 10 }}>
             <Box sx={{ backgroundColor: '#fff', borderRadius: '20px', p: 3, mb: 3 }}>
-                <Box component="form" sx={{ mb: 3, pb: 3 }}>
-                    <Grid container spacing={3} justifyContent="center">
+                <Box component="form" sx={{ mb: 3,pb:1 }}>
+                    <Grid container spacing={3} justifyContent="center" sx = {{pb:1}}>
                         <Grid item xs={12} sm={6} md={6}>
                             <TextField
                                 fullWidth
                                 label="Nome"
-                                variant="outlined"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                value={filters.tempName}
-                                onChange={(e) => setFilters({ ...filters, tempName: e.target.value })}
                                 sx={{ backgroundColor: '#f0f0f0' }}
+                                variant="outlined"
+                                InputLabelProps={{ 
+                                    shrink: true, 
+                                }}
+                                value={nome}
+                                onChange={(e) => setNome(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6} md={6}>
                             <TextField
                                 fullWidth
+                                sx={{ backgroundColor: '#f0f0f0' }}
                                 label="Promovido Por"
                                 variant="outlined"
-                                InputLabelProps={{
-                                    shrink: true,
+                                InputLabelProps={{ 
+                                    shrink: true, 
                                 }}
-                                value={filters.tempPromotedBy}
-                                onChange={(e) => setFilters({ ...filters, tempPromotedBy: e.target.value })}
-                                sx={{ backgroundColor: '#f0f0f0' }}
+                                value={promovidoPor}
+                                onChange={(e) => setPromovidoPor(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6} md={4}>
                             <TextField
                                 fullWidth
+                                sx={{ backgroundColor: '#f0f0f0' }}
                                 label="Data de Início"
                                 variant="outlined"
                                 type="date"
-                                InputLabelProps={{
+                                InputLabelProps={{ 
                                     shrink: true
                                 }}
-                                value={filters.tempInitialDate}
-                                onChange={(e) => setFilters({ ...filters, tempInitialDate: e.target.value })}
-                                sx={{ backgroundColor: '#f0f0f0' }}
+                                value={dataInicio}
+                                onChange={(e) => setDataInicio(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6} md={4}>
                             <TextField
                                 fullWidth
+                                sx={{ backgroundColor: '#f0f0f0' }}
                                 label="Data de Fim"
                                 variant="outlined"
                                 type="date"
-                                InputLabelProps={{
-                                    shrink: true
+                                InputLabelProps={{ 
+                                    shrink: true 
                                 }}
-                                value={filters.tempFinalDate}
-                                onChange={(e) => setFilters({ ...filters, tempFinalDate: e.target.value })}
-                                sx={{ backgroundColor: '#f0f0f0' }}
+                                value={dataFim}
+                                onChange={(e) => setDataFim(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6} md={4} container justifyContent="center" alignItems="center">
@@ -133,52 +118,58 @@ function Eventos() {
                                         backgroundColor: '#0c78f3',
                                     },
                                 }}
-                                onClick={handleFilter}
+                                onClick={handleSearch}
                             >
                                 Carregar Eventos
                             </Button>
                         </Grid>
                     </Grid>
+                    <Typography variant="body2" color="textSecondary" sx={{ mb: 1, fontSize: '0.80rem' }}>
+                            * Para filtrar por data, informe tanto a data de início como a de fim do evento *
+                        </Typography>
                 </Box>
+
                 {showTable && (
-                    <TableContainer component={Box} sx={{ borderRadius: '8px', border: '1px solid #CFCECE' }}>
-                        {loading && <CircularProgress />}
-                        {error && <Typography color="error" align="center">Erro ao carregar dados: {error.message}</Typography>}
-                        {!loading && !error && (
-                            <Table>
-                                <TableHead>
-                                    <TableRow style={{ backgroundColor: '#f0f0f0' }}>
-                                        <TableCell style={{ borderRight: '1px solid #CFCECE', width: '25%', fontWeight: 'bold', paddingLeft: '20px' }}>
-                                            Nome
-                                        </TableCell>
-                                        <TableCell style={{ borderRight: '1px solid #CFCECE', width: '25%', textAlign: 'center', fontWeight: 'bold' }}>
-                                            Promovido Por
-                                        </TableCell>
-                                        <TableCell style={{ borderRight: '1px solid #CFCECE', width: '15%', textAlign: 'center', fontWeight: 'bold' }}>
-                                            Data Inicial
-                                        </TableCell>
-                                        <TableCell style={{ borderRight: '1px solid #CFCECE', width: '15%', textAlign: 'center', fontWeight: 'bold' }}>
-                                            Data Final
-                                        </TableCell>
-                                        <TableCell style={{ width: '20%', textAlign: 'center', fontWeight: 'bold' }}>
-                                            Informações do evento
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {filteredEvents.map((event) => (
-                                        <TableRow key={event.id}>
-                                            <TableCell style={{ borderRight: '1px solid #CFCECE', paddingLeft: '20px' }}>{event.name}</TableCell>
-                                            <TableCell style={{ borderRight: '1px solid #CFCECE', textAlign: 'center' }}>{event.promoted_by}</TableCell>
-                                            <TableCell style={{ borderRight: '1px solid #CFCECE', textAlign: 'center' }}>{event.initial_date}</TableCell>
-                                            <TableCell style={{ borderRight: '1px solid #CFCECE', textAlign: 'center' }}>{event.final_date}</TableCell>
-                                            <TableCell style={{ textAlign: 'center' }}><ModalPapers /></TableCell>
+                    <>
+                        <TableContainer component={Box} sx={{ borderRadius: '8px', border: '1px solid #CFCECE' }}>
+                            {loading && <CircularProgress />}
+                            {error && <Typography color="error" align="center">Erro ao carregar dados: {error.message}</Typography>}
+                            {!loading && !error && (
+                                <Table>
+                                    <TableHead>
+                                        <TableRow style={{ backgroundColor: '#f0f0f0' }}>
+                                            <TableCell style={{ borderRight: '1px solid #CFCECE', width: '25%', fontWeight: 'bold', paddingLeft: '20px' }}>
+                                                Nome
+                                            </TableCell>
+                                            <TableCell style={{ borderRight: '1px solid #CFCECE', width: '25%', textAlign: 'center', fontWeight: 'bold' }}>
+                                                Promovido Por
+                                            </TableCell>
+                                            <TableCell style={{ borderRight: '1px solid #CFCECE', width: '15%', textAlign: 'center', fontWeight: 'bold' }}>
+                                                Data Inicial
+                                            </TableCell>
+                                            <TableCell style={{ borderRight: '1px solid #CFCECE', width: '15%', textAlign: 'center', fontWeight: 'bold' }}>
+                                                Data Final
+                                            </TableCell>
+                                            <TableCell style={{ width: '20%', textAlign: 'center', fontWeight: 'bold' }}>
+                                                Informações do evento
+                                            </TableCell>
                                         </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        )}
-                    </TableContainer>
+                                    </TableHead>
+                                    <TableBody>
+                                        {events.map((event) => (
+                                            <TableRow key={event.id}>
+                                                <TableCell style={{ borderRight: '1px solid #CFCECE', paddingLeft: '20px' }}>{event.name}</TableCell>
+                                                <TableCell style={{ borderRight: '1px solid #CFCECE', textAlign: 'center' }}>{event.promoted_by}</TableCell>
+                                                <TableCell style={{ borderRight: '1px solid #CFCECE', textAlign: 'center' }}>{event.initial_date}</TableCell>
+                                                <TableCell style={{ borderRight: '1px solid #CFCECE', textAlign: 'center' }}>{event.final_date}</TableCell>
+                                                <TableCell style={{ textAlign: 'center' }}><ModalPapers /></TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            )}
+                        </TableContainer>
+                    </>
                 )}
             </Box>
         </Box>
