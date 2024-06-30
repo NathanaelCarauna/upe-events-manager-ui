0689016e492ebed8ef62feb7495c01515e4f641e
lgr4 <lgr1@poli.br>
2024-06-30 18:03:32 -0300

    #139 Implementar plot de gráficos

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import Button from "@mui/material/Button";
-import DemoPaper from "@mui/material/Paper";
-import Event from "@mui/icons-material/Event";
+import Paper from "@mui/material/Paper";
+import EventIcon from "@mui/icons-material/Event";
 import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
-import { toast } from "react-toastify";
 import ErrorService from "../services/errorService";
+import Graph from '../components/DashboardGraph';
+import { Box } from '@mui/material';
 
 function Dashboard() {
-    
+
   const handleSomeAction = async () => {
     const error = {
       response: {
@@ -19,30 +21,33 @@ function Dashboard() {
     ErrorService.handleError(error);
   };
 
-return(
-        <>
-        <a href="/eventos">
-            <Button>
-                <DemoPaper square={false}>
-                    <Event/>
-                    <p>Eventos</p>
-                </DemoPaper>
-            </Button>
+  return (
+    <>
+    <Box sx={{ pt: 2, pb: 2, pl: 5, pr: 5 }}>
+      <a href="/eventos">
+          <Button>
+            <Paper square={false}>
+              <EventIcon />
+              <p>Eventos</p>
+            </Paper>
+          </Button>
         </a>
 
         <a href="/papers">
-            <Button>
-                <DemoPaper square={false}>
-                    <FormatListNumberedIcon/>
-                    <p>Papers</p>
-                </DemoPaper>
-            </Button>
+          <Button>
+            <Paper square={false}>
+              <FormatListNumberedIcon />
+              <p>Papers</p>
+            </Paper>
+          </Button>
         </a>
 
         <button onClick={handleSomeAction}>Clique para acionar erro</button>
         
-        </>
-    )
+        <Graph/>
+    </Box>
+    </>
+  );
 }
 
 export default Dashboard;
